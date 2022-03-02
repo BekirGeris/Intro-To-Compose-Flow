3b29caaa91258a3eeedb39a88f169659050b39aa
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2022-03-03 00:50:23 +0300

    Flowun özellikleri sınandı

--- app/src/main/java/com/example/composeflow/HomeViewMoıdel.kt
+++ app/src/main/java/com/example/composeflow/HomeViewMoıdel.kt
@@ -1,8 +1,10 @@
 package com.example.composeflow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class HomeViewMoıdel : ViewModel() {
 
@@ -12,11 +14,40 @@ class HomeViewMoıdel : ViewModel() {
 
         emit(coundDownFrom)
 
-        while (counter > 0){
+        while (counter > 0) {
             delay(1000)
             counter--
             emit(counter)
         }
     }
 
+    init {
+        collectInViewModel()
+    }
+
+    private fun collectInViewModel() {
+        viewModelScope.launch {
+            countDownTimerFlow
+                .filter {
+                    it % 3 == 0
+                }
+                .map {
+                    it * it
+                }
+                .onEach {
+                }
+                .collect {
+                println("counter : ${it}")
+            }
+
+            countDownTimerFlow.collectLatest {
+                delay(2000)//her gönderilen veriden sonra 2 sn bekler eğer 2 sn geçmeden yeni bir veri gelirse bu sefer onun için 2sn bekler
+                println("counter : ${it}")
+            }
+        }
+
+        countDownTimerFlow.onEach {
+            println(it)
+        }.launchIn(viewModelScope)
+    }
 }
